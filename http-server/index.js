eb0c8875e04ff2a79830cf82138eb12d3bfb52fc
ekta <ekta@LAPTOP-3E3JDLTJ>
2023-12-14 00:34:37 +0530

    Milestone

---
+++ http-server/index.js
@@ -0,0 +1,57 @@
+const http = require("http");
+const fs = require("fs");
+const args = require('minimist')(process.argv.slice(2));
+
+const port = args.port;
+
+console.log('Port:', port);
+
+let homeContent = "";
+let projectContent = "";
+let registerContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, register) => {
+  if (err) {
+    throw err;
+  }
+  registerContent = register;
+});
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(registerContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+
+  
