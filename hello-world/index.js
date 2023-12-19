9be3fa392edb743613eab44fc8cef901a6bc40ba
ekta <ekta@LAPTOP-3E3JDLTJ>
2023-12-19 22:31:19 +0530

    message

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,3 +1,5 @@
-const args = require("minimist")(process.argv.slice(2));
-
-console.log(args);
+const hello = () => {
+    console.log("Hello GitHub!");
+  };
+  
+  hello();
---
+++ index.js
@@ -0,0 +1,7 @@
+const hello = 'Hello'
+
+const returnHello = () => {
+  console.log(hello)
+}
+
+returnHello()
